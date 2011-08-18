ecdc0f9b61815b3d81ad5251743aaedd80f3888b
Pedro Algarvio <pedro@algarvio.me>
2011-08-18 20:32:29 +0100

    Allow passing the `id` of the menu entry and also classes to the `li` element, plus, allow non link content to be added to the menus.

--- flaskext/menubuilder.py
+++ flaskext/menubuilder.py
@@ -34,16 +34,44 @@ class MenuBuilder(object):
 
     def add_menu_entry(self, menu_id, title, endpoint, priority=0,
                        activewhen=NEVER, visiblewhen=ANYTIME, classes=None,
-                       **html_opts):
+                       id=None, **html_opts):
         menu_item = MenuItem(
             title, endpoint, priority=priority, activewhen=activewhen,
             visiblewhen=visiblewhen, classes=classes, builder=self.builder,
-            **html_opts
+            id=id, **html_opts
         )
         self.__add_menu_item(menu_id, menu_item)
 
     def add_menu_item(self, menu_id, menu_item):
         self.__add_menu_item(menu_id, menu_item)
+
+    def has_menu(self, menu_id):
+        return menu_id in self.menus
+
+    def has_menu_endpoint(self, endpoint, menu_id=None):
+        if menu_id:
+            return endpoint in self.menus[menu_id]
+        for menu_id in self.menus.keys():
+            if endpoint in self.menus[menu_id]:
+                return True
+        return False
+
+    def has_item_by_id(self, item_id, menu_id=None):
+        if menu_id:
+            for menu_item in self.menus[menu_id].values():
+                if menu_item.id is None:
+                    continue
+                if menu_item.id == item_id:
+                    return True
+            return False
+
+        for menu_id in self.menus.keys():
+            for menu_item in self.menus[menu_id].values():
+                if menu_item.id is None:
+                    continue
+                if menu_item.id == item_id:
+                    return True
+        return False
 
     def render(self, menu_id, type='ol'):
         assert type in ('ul', 'ol')
@@ -70,13 +98,13 @@ class MenuBuilder(object):
             rendered_entry = entry.render()
             if not rendered_entry:
                 continue
-            yield self.builder.li(rendered_entry)
+            yield rendered_entry
 
 
 class MenuItem(object):
     def __init__(self, title, endpoint, priority=0, activewhen=NEVER,
                  visiblewhen=ANYTIME, classes=None, format=None, builder=None,
-                 **html_opts):
+                 id=None, li_classes=None, **html_opts):
         self.title = title
         self.endpoint = endpoint
         self.priority = priority
@@ -84,6 +112,8 @@ class MenuItem(object):
         self.visiblewhen = visiblewhen
         if classes is None:
             classes = []
+        elif isinstance(classes, basestring):
+            classes = [classes]
         self.classes = classes
         if format and not builder:
             assert format in ('html', 'xhtml')
@@ -94,6 +124,12 @@ class MenuItem(object):
             self.builder = getattr(werkzeug.utils, 'html')
         else:
             raise RuntimeError("You can only pass one of \"format\" and \"builder\"")
+        self.id = id
+        if li_classes is None:
+            li_classes = []
+        elif isinstance(li_classes, basestring):
+            li_classes = [li_classes]
+        self.li_classes = li_classes
         self.html_opts = html_opts
 
     def render(self):
@@ -103,20 +139,36 @@ class MenuItem(object):
             return
 
         opts = {'href': url_for(self.endpoint)}
+        classes, li_classes = self.build_classes()
+        opts['class_'] = ' '.join(set(classes + self.classes))
+        if self.id:
+            opts['id'] = self.id
+
+        li_classes = list(set(self.li_classes + li_classes))
+        if li_classes:
+            li_opts = {'class_': ' '.join(list(set(self.li_classes + li_classes)))}
+        else:
+            li_opts = {}
+
+        opts.update(self.html_opts)
+        return self.builder.li(self.builder.a(self.title, **opts), **li_opts)
+
+    def build_classes(self):
         classes = []
+        li_classes = []
         if self.activewhen is NEVER:
             classes.append('inactive')
+            li_classes.append('inactive')
         elif self.activewhen is ANYTIME:
             classes.append('active')
+            li_classes.append('active')
         elif callable(self.activewhen) and self.activewhen(self):
             classes.append('active')
+            li_classes.append('active')
         else:
             classes.append('inactive')
-        if self.classes:
-            classes.extend(self.classes)
-        opts['class_'] = ' '.join(set(classes))
-        opts.update(self.html_opts)
-        return self.builder.a(self.title, **opts)
+            li_classes.append('active')
+        return classes, li_classes
 
     def __lt__(self, other):
         if self.priority == other.priority:
@@ -155,3 +207,43 @@ class MenuItem(object):
     def __repr__(self):
         return self.__unicode__().encode('utf-8')
 
+
+class MenuItemContent(MenuItem):
+    def __init__(self, content, title=None, endpoint=None, priority=0, activewhen=NEVER,
+                 visiblewhen=ANYTIME, classes=None, format=None, builder=None, id=None,
+                 li_classes=None, is_link=True, **html_opts):
+        MenuItem.__init__(
+            self, title, endpoint, priority=priority, activewhen=activewhen,
+            visiblewhen=visiblewhen, classes=classes, format=format,
+            builder=builder, id=id, li_classes=li_classes, **html_opts
+        )
+        self.content = content
+        self.is_link = is_link
+
+    def render(self):
+        if self.visiblewhen is NEVER:
+            return
+        elif callable(self.visiblewhen) and not self.visiblewhen(self):
+            return
+        opts = {}
+        if self.is_link:
+            opts['href'] = url_for(self.endpoint)
+            if self.title:
+                opts['alt'] = opts['title'] = self.title
+        classes, li_classes = self.build_classes()
+        opts['class_'] = ' '.join(set(classes))
+        opts.update(self.html_opts)
+
+        li_classes = list(set(self.li_classes + li_classes))
+        if li_classes:
+            li_opts = {'class_': ' '.join(list(set(self.li_classes + li_classes)))}
+        else:
+            li_opts = {}
+
+        if self.is_link:
+            element = self.builder.a
+        else:
+            element = self.builder.span
+        if self.id:
+            opts['id'] = self.id
+        return self.builder.li(element(self.content, **opts), **li_opts)
